eab26787b8468fe4ace1c0cb99c49a38072d31fe
Hiren <rebeldevmurari@gmail.com>
2023-07-03 11:52:31 +0100

    added delete comment function, successfully tested and working fine, will delete with the help of comment id

--- src/app/compomnents/admin/view-comments/view-comments.component.ts
+++ src/app/compomnents/admin/view-comments/view-comments.component.ts
@@ -13,9 +13,12 @@ export class ViewCommentsComponent implements OnInit
 {
   postId = ' ';
 
+  commentId = '';
+
   comments = 
   [
     {
+      coId: '',
       content: '',
       user:
       {
@@ -49,4 +52,43 @@ export class ViewCommentsComponent implements OnInit
     });    
       
   }
+
+//Deleting selected comment
+    deleteCommentFun(commentId:any)
+    {  
+      Swal.fire({
+        icon: 'question',
+        title: 'Are you sure???',
+        confirmButtonText: 'Delete',
+        showCancelButton: true,
+      }).then((result)=>
+      {
+
+    //calling delete function
+      if(result.isConfirmed)
+      {
+        this.commentService.deleteComment(commentId).subscribe({
+          next: (data:any)=>
+          { 
+      //below function will help page to do the filter and get the accurate data after deletation 
+            console.log(data);
+            this.comments = this.comments.filter((comment) => comment.coId != commentId)
+            Swal.fire('Success', 'Quiz successfully deleted !!', 'success');
+          
+            
+          },
+          error: (error)=>
+          {
+            console.log(error);
+            Swal.fire('Error', 'Something went wrong with deleteing comment !! ', 'error');
+
+          }
+        });
+      }
+
+
+    });
+
+    }  
+     
 }
