77feba3f55804f8ee57b9e92c6085f72bdda2e0a
Hiren <rebeldevmurari@gmail.com>
2023-07-13 23:06:12 +0100

    same

--- src/app/compomnents/user/comment-operations/comment-operations.component.ts
+++ src/app/compomnents/user/comment-operations/comment-operations.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommentService } from 'src/app/service/comment.service';
+import { LoginServiceService } from 'src/app/service/login-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comment-operations',
@@ -11,12 +14,56 @@ export class CommentOperationsComponent implements OnInit
 
   postId = '';
 
-  constructor(private route:ActivatedRoute) {}
+  comments = 
+  [
+    {
+      coId: '',
+      content: '',
+      user:
+      {
+        id: '',
+        name: '',
+        email: '',
+      },
+     
+    }
+  ]
+
+  user = 
+  {
+    id : '',
+    name: '',
+    email: '',
+    pass: '',
+    about: '',
+  }
+
+  constructor(private route:ActivatedRoute,
+              private commentService:CommentService,
+              private userService:LoginServiceService) {}
 
   ngOnInit(): void 
   {
     this.postId = this.route.snapshot.params['poId'];
     console.log(this.postId);
+
+//Fetching list of comments for specific Post with it;s user---------------------------------------------------
+    this.commentService.getListOfCommByPo(this.postId).subscribe({
+      next : (data:any)=>
+      {
+        this.comments = data;
+      },
+      error: (error)=>
+      {
+        Swal.fire('Error', 'error with fetching comments data !!', 'error');
+      }
+    });
+
+//Get User Details----------------------------------------------------------------------------------------
+    if(this.userService.isLoggedIn() == true)
+    {
+        this.user = this.userService.getUser();
+    }
     
       
   }
